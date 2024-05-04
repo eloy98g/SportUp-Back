5118e4a9c1fbb2eb76c7f20165a7fe1e3fcb8431
Eloy Gómez García <eloygomezgarcia@MacBook-Air-de-Eloy.local>
2024-05-05 00:35:38 +0200

    minor change

--- src/models/application/methods/create.ts
+++ src/models/application/methods/create.ts
@@ -3,13 +3,21 @@ import { connection } from "../../dbConnection";
 export async function create(input: any) {
   const { activityGid, userGid } = input;
 
-  const { rows } = await connection.execute({
-    sql: "SELECT gid FROM application WHERE userGid = ? AND activityGid = ?;",
+  const { rows:userTeam } = await connection.execute({
+    sql: "SELECT teamGid FROM user_team WHERE userGid = ? AND activityGid = ?;",
     args: [userGid, activityGid],
   });
 
-  if (rows.length > 0)
-    return { result: false, message: "Error: Este usuario ya había solicitado unirse" };
+  const { rows:application } = await connection.execute({
+    sql: "SELECT gid FROM application WHERE userGid = ? AND activityGid = ? AND status = 'pending';",
+    args: [userGid, activityGid],
+  });
+
+  if (userTeam.length > 0)
+    return { result: false, message: "Este usuario ya pertenece a la actividad" };
+
+  if (application.length > 0)
+    return { result: false, message: "Este usuario ya ha solicitado unirse" };
 
   const date = Date.now();
   const sql = `INSERT INTO application (creationDate, activityGid, userGid, status) VALUES (?, ?, ?, 'pending');`;
