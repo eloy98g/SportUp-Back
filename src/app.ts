80a2805e8fb00b9e0a8480a5d4e5a86094964e28
Eloy Gómez García <eloygomezgarcia@MacBook-Air-de-Eloy.local>
2024-05-15 10:41:40 +0200

    conexion working

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, json } from "express";
 import http from "http";
-import { Server as SocketServer } from "socket.io";
+import { Server } from "socket.io";
+import CustomSocket from "./types/socket"; 
 
 // Middlewares
 import { corsMiddleware } from "./middlewares/cors";
@@ -17,12 +18,48 @@ const app: Application = express();
 export const PORT = process.env.PORT || 1234;
 
 const server = http.createServer(app);
-const io = new SocketServer(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
+io.use((socket: CustomSocket, next) => {
+  const auth = socket.handshake.headers['authorization'];
+  const chat = socket.handshake.headers['chat'];
 
-io.on('connection', (socket) => {
-  console.log('\n\n',socket,'\n\n')
-  console.log('a user connected');
+  if (!auth) {
+    return next(new Error('authentication error'));
+  }
+
+  const gid = auth.split(' ')[1]; // Suponiendo que el token está en el formato "Bearer <token>"
+  const chatGid  = chat
+
+  if (!gid || gid === 'null' || !chatGid) {
+    return next(new Error('invalid gid'));
+  }
+
+  socket.gid = gid
+  socket.chatGid = chatGid
+  next();
+});
+
+io.on('connection', (socket: CustomSocket) => {
+  console.log('a user connected with gid:', socket.gid);
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('message:create', (msg) => {
+    console.log('message: ' + msg);
+    socket.broadcast.emit('message', msg);
+  });
+
+  socket.on('error', (err) => {
+    console.error('Socket error:', err);
+  });
 });
 
 app.use(json());
@@ -36,6 +73,6 @@ app.use("/chat", chatRouter);
 app.use("/sport", sportRouter);
 app.use("/application", applicationRouter);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server listening on http://localhost:${PORT}`);
 });
