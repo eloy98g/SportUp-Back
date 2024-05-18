747b2a1bd6be10830bed0c3facef71fe746e141b
Eloy Gómez García <eloygomezgarcia@MacBook-Air-de-Eloy.local>
2024-05-18 19:16:10 +0200

    report bug fix

--- src/schemas/report/index.ts
+++ src/schemas/report/index.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const reportSchema = z.object({
   userGid: z.string(),
   reportedBy: z.string(),
-  reportGid: z.string(),
+  reportReason: z.string(),
 });
 
 export function validateReport(input: any) {
